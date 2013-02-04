11f6d6e4398ad29a43d82436c897b53e71d6d291
Alexander Christiaan Jacob Dekker <github@acjs.net>
2013-02-04 22:32:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,5 +18,17 @@ if(document.getElementById)
 
         context.fillStyle = 'rgb(' + Math.round(Math.random() * 255) + ', 0, 0)';
         context.fillRect(width1, 0, width2, canvas.height);
+        
+        // Myrthe zegt "hoi".
+        context.fillStyle = 'rgb(127, 0, 255)';
+        context.beginPath();
+        context.moveTo(75,40);
+        context.bezierCurveTo(75,37,70,25,50,25);
+        context.bezierCurveTo(20,25,20,62.5,20,62.5);
+        context.bezierCurveTo(20,80,40,102,75,120);
+        context.bezierCurveTo(110,102,130,80,130,62.5);
+        context.bezierCurveTo(130,62.5,130,25,100,25);
+        context.bezierCurveTo(85,25,75,37,75,40);
+        context.fill();
     }
 }
