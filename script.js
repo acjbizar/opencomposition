bc2de5de14398928f6130f4b8a5cfbbc7772ba55
Alexander Christiaan Jacob Dekker <github@acjs.net>
2013-02-14 01:24:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -53,29 +53,30 @@ if(document.getElementById)
         context.fillRect(width1, 0, width2, canvas.height);
         
         // Myrthe zegt "hoi".
-    			var no_of_hearts= 2;
-				for(var i= 0; i < no_of_hearts; i++){
-					var x= Math.round(75+Math.random()*(canvas.width-150));
-					var y= Math.round(40+Math.random()*(canvas.height-80));
-					if(x > width1){	//on the right half of the screen
+    	var no_of_hearts= 2;
+		for(var i= 0; i < no_of_hearts; i++){
+		    var x= Math.round(75+Math.random()*(canvas.width-150));
+			var y= Math.round(40+Math.random()*(canvas.height-80));
+			if(x > width1){	//on the right half of the screen
 		        //red tinted hearts:
 		        context.fillStyle = 'rgb(' + Math.round(Math.random() * 255) + ', 0, 0)';
-					} else {	//on the left half of the screen
+			} else {	//on the left half of the screen
 		        //green tinted hearts
 		        context.fillStyle = 'rgb(127,' + Math.round(Math.random() * 255) + ', 0)';
-					}
+			}
 	        //purple hearts:	context.fillStyle = 'rgb(127, 0, 255)';
 	        context.beginPath();
 	        context.moveTo(x,y);		//(75,40);
-	        context.bezierCurveTo(x,y-3,				x-5,y-15,			x-25,y-15);
-	        context.bezierCurveTo(x-55,y-15,		x-55,y+22.5,	x-55,y+22.5);
-	        context.bezierCurveTo(x-55,y+40,		x-35,y+62,		x,y+80);
-	        context.bezierCurveTo(x+45,y+62,		x+55,y+40,		x+55,y+22.5);
-	        context.bezierCurveTo(x+55,y+22.5,	x+55,y-15,		x+25,y-15);
-	        context.bezierCurveTo(x+10,y-15,		x,y-3,				x,y);
+	        context.bezierCurveTo(x,y-3, x-5,y-15, x-25,y-15);
+	        context.bezierCurveTo(x-55,y-15, x-55,y+22.5, x-55,y+22.5);
+	        context.bezierCurveTo(x-55,y+40, x-35,y+62, x,y+80);
+	        context.bezierCurveTo(x+45,y+62, x+55,y+40, x+55,y+22.5);
+	        context.bezierCurveTo(x+55,y+22.5, x+55,y-15, x+25,y-15);
+	        context.bezierCurveTo(x+10,y-15, x,y-3, x,y);
+            context.fill();
 
-*
             // Myrthe zegt "Happy V-day, baby!"
+            // ACJ zegt "Thanks, babe! :D <3"
 	        context.fillStyle = 'rgb(127, 0, 255)';
 	        context.beginPath();
 	        context.moveTo(75,40);
@@ -85,8 +86,8 @@ if(document.getElementById)
 	        context.bezierCurveTo(110,102,130,80,130,62.5);
 	        context.bezierCurveTo(130,62.5,130,25,100,25);
 	        context.bezierCurveTo(85,25,75,37,75,40);
-*/
+
 	        context.fill();
-				}
+		}
     }
 }
