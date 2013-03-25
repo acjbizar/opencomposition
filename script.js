1d0d45471ee8cd38ef8442e7a7485d4331d8ebf5
lock2 <ctau98@hotmail.com>
2013-03-25 17:16:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,95 @@
-if(document.getElementById)
-{
-    canvas = document.getElementById('content');
+(function(window, undefined){
+	window.Lock632= function(canvas){
+		var canvas= canvas;
+		var context= canvas.getContext('2d');
+    //orange background
+   	context.fillStyle = 'orange';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+		//draw the head
+		head(canvas);
 
-    if(canvas.getContext('2d'))
-    {
-        context = canvas.getContext('2d');
+    canvas.addEventListener('mousemove', function(evt){
+      var mousePos= mouse_pos(evt);
+			//redraw the head and the face at movements of the mouse pointer
+      head();
+      face(calculate_xy(mousePos));
+    }, false);
 
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+		//draw the head
+		function head(){
+			context.beginPath();
+			context.fillStyle= 'white';
+			context.arc(Math.round(canvas.width/2), canvas.height, 210, 0, 2*Math.PI);
+			context.fill();
+  	}
+  	
+		//draw the face
+  	function face(eye_xy){
+			//eyes
+			var x_eye_1= eye_xy.x;
+			var y_eye_1= eye_xy.y;
 
-        var width1 = Math.floor(Math.random() * canvas.width);
-        var width2 = canvas.width - width1;
-
-        context.fillStyle = 'rgb(' + Math.round(Math.random() * 255) + ', 0, 0)';
-        context.fillRect(0, 0, width1, canvas.height);
-
-        context.fillStyle = 'rgb(' + Math.round(Math.random() * 255) + ', 0, 0)';
-        context.fillRect(width1, 0, width2, canvas.height);
-        
-        // Myrthe zegt "hoi".
-        context.fillStyle = 'rgb(127, 0, 255)';
-        context.beginPath();
-        context.moveTo(75,40);
-        context.bezierCurveTo(75,37,70,25,50,25);
-        context.bezierCurveTo(20,25,20,62.5,20,62.5);
-        context.bezierCurveTo(20,80,40,102,75,120);
-        context.bezierCurveTo(110,102,130,80,130,62.5);
-        context.bezierCurveTo(130,62.5,130,25,100,25);
-        context.bezierCurveTo(85,25,75,37,75,40);
-        context.fill();
+			var dx_between_eyes= 0;	//you can vary this variable to have funny eyes
+			
+			context.beginPath();
+			context.fillStyle = '614A3A';
+			context.arc(x_eye_1, y_eye_1, 30, 0, 2*Math.PI);
+			context.arc(x_eye_1+150-dx_between_eyes, y_eye_1, 30, 0, 2*Math.PI);
+			context.fill();
+			//mouth
+			context.beginPath();
+			context.fillStyle = 'cc3333';
+			context.arc(x_eye_1+75, y_eye_1+130, 50, 0, 2*Math.PI);
+			context.fill();
+	  }
+	  //calculate the position of one eye (right eye/ left on screen)
+	  function calculate_xy(mousePos){
+			var x_eye_1= Math.round(canvas.width/2)-80;
+			var dx= Math.round((mousePos.x-x_eye_1))/30;
+			//return x_eye_1+dx;
+			var y_eye_1= Math.round(canvas.height)-120;
+			var dy= Math.round((mousePos.y-y_eye_1))/25;
+			//return y_eye_1+dy;
+	    return {
+	      x: x_eye_1+dx,
+	      y: y_eye_1+dy,
+	      dx: dx,
+	      dy: dy
+	    };
+	  }
+		/**
+		 * mouse position
+		 */
+    function mouse_pos(evt){
+      var rect= canvas.getBoundingClientRect();
+      return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+      };
     }
-}
-        
-if(document.getElementById)
-{
-    canvas = document.getElementById('content');
 
-    if(canvas.getContext('2d'))
-    {
-        context = canvas.getContext('2d');
+		//the methods going public
+	  return {
+			//head: head,
+			//face: face
+	  };
+	}
+	
+})(window);
 
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
 
-        var width1 = Math.floor(Math.random() * canvas.width);
-        var width2 = canvas.width - width1;
 
-        context.fillStyle = 'rgb(' + Math.round(Math.random() * 255) + ', 0, 0)';
-        context.fillRect(0, 0, width1, canvas.height);
 
-        context.fillStyle = 'rgb(' + Math.round(Math.random() * 255) + ', 0, 0)';
-        context.fillRect(width1, 0, width2, canvas.height);
-        
-        // Myrthe zegt "hoi".
-    	var no_of_hearts= 2;
-		for(var i= 0; i < no_of_hearts; i++){
-		    var x= Math.round(75+Math.random()*(canvas.width-150));
-			var y= Math.round(40+Math.random()*(canvas.height-80));
-			if(x > width1){	//on the right half of the screen
-		        //red tinted hearts:
-		        context.fillStyle = 'rgb(' + Math.round(Math.random() * 255) + ', 0, 0)';
-			} else {	//on the left half of the screen
-		        //green tinted hearts
-		        context.fillStyle = 'rgb(127,' + Math.round(Math.random() * 255) + ', 0)';
-			}
-	        //purple hearts:	context.fillStyle = 'rgb(127, 0, 255)';
-	        context.beginPath();
-	        context.moveTo(x,y);		//(75,40);
-	        context.bezierCurveTo(x,y-3, x-5,y-15, x-25,y-15);
-	        context.bezierCurveTo(x-55,y-15, x-55,y+22.5, x-55,y+22.5);
-	        context.bezierCurveTo(x-55,y+40, x-35,y+62, x,y+80);
-	        context.bezierCurveTo(x+45,y+62, x+55,y+40, x+55,y+22.5);
-	        context.bezierCurveTo(x+55,y+22.5, x+55,y-15, x+25,y-15);
-	        context.bezierCurveTo(x+10,y-15, x,y-3, x,y);
-            context.fill();
+if(document.getElementById){
+  canvas = document.getElementById('content');
 
-            // Myrthe zegt "Happy V-day, baby!"
-            // ACJ zegt "Thanks, babe! :D <3"
-	        context.fillStyle = 'rgb(127, 0, 255)';
-	        context.beginPath();
-	        context.moveTo(75,40);
-	        context.bezierCurveTo(75,37,70,25,50,25);
-	        context.bezierCurveTo(20,25,20,62.5,20,62.5);
-	        context.bezierCurveTo(20,80,40,102,75,120);
-	        context.bezierCurveTo(110,102,130,80,130,62.5);
-	        context.bezierCurveTo(130,62.5,130,25,100,25);
-	        context.bezierCurveTo(85,25,75,37,75,40);
+  if(canvas.getContext('2d')){
+    context= canvas.getContext('2d');
 
-	        context.fill();
-		}
-    }
+    canvas.width= window.innerWidth;
+    canvas.height= window.innerHeight;
+
+		//CT Au :-)
+		var lock632= new Lock632(canvas);
+  }
 }
